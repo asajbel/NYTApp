a9159c9813f0ae0edeb8824de2fa1110674fb112
nchandika1 <nchandika@gmail.com>
2017-10-20 13:55:43 -0700

    minor fixes

--- javascript.js
+++ javascript.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
 	$("#search").on("submit", function(event) {
 		event.preventDefault();
 		console.log(event.currentTarget);
+
 		var searchTerm = event.currentTarget[0].value;
 		var numRecords = event.currentTarget[1].value;
 		var startYear = event.currentTarget[2].value;
@@ -16,8 +17,10 @@ $(document).ready(function () {
 		// Validate Year variables
 		startYear += "0101";
 		endYear += "1231";
-		queryURL += "&begin_date=" + startYear;
-		queryURL += "&end_date=" + endYear;
+		if (startYear != "")
+			queryURL += "&begin_date=" + startYear;
+		if (endYear != "")
+			queryURL += "&end_date=" + endYear;
 
 		console.log(queryURL);
 
@@ -25,26 +28,31 @@ $(document).ready(function () {
 			console.log(result);
 
 			var resultDocs = result.response.docs;
+			var dataSize = result.response.docs.length;
+
+			if (numRecords < dataSize)
+				numRecords = dataSize;
+
 			for (var i =0; i<numRecords; i++) {
 				var value = resultDocs[i];
 
 				//  Create the elements
-
-				var article = new $("<div>");
+				var article = $("<div>");
 				article.addClass("article");
-				var headLine = new $("<h3>").text(value.headline.main);
+
+				var headLine = $("<h3>").text(value.headline.main);
 				article.append(headLine);
 
-				var byLine = new $("<h5>").text(value.by_line.original);
+				var byLine = $("<h5>").text(value.by_line.original);
 				article.append(byLine);
 
-				var section = new $("<h5>").text("Section: " + value.section);
+				var section = $("<h5>").text("Section: " + value.section);
 				article.append(section);
 
-				var pubDate = new $("<h5>").text(value.pub_date);
+				var pubDate = $("<h5>").text(value.pub_date);
 				article.append(pubDate);
 
-				var url = new $("<a>").text(value.web_url);
+				var url = $("<a>").text(value.web_url);
 				url.attr("href", value.web_url);
 				url.attr("target", "_blank");
 				article.append(url);
